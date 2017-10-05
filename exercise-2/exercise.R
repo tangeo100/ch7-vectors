8491b60034fe1db4100f53a9cbeb232c159d5f6e
tangeo100 <rnld.tang@gmail.com>
2017-10-05 15:14:59 -0700

    change exercise.R in exercise-1, exercise-2, and exercise-3

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,26 +1,41 @@
 # Exercise 2: Subsetting and Manipulating Vectors
 
 # Create a vector `x` that the numbers 5,2,6,2,1,7
-
+x <- c(5, 2, 6, 2, 1, 7)
 
 # Create a vector `y` that has the numbers 2,3
-
+y <- c(2, 3)
 
 # Create a vector `z` by adding (not combining, but adding) `x` and `y` (note the recycling!)
-
+z <- x + y
 
 # Create a vector `first.three` that has the first three elements of `z` in it
-
+first.three <- z[c(1:3)]
 
 # Create a vector `small` that has the values of `z` that are less than 5
-
+small <- z[z < 5]
 
 # Create a vector `large` that has the values of `z` that are greater than or equal to 5
-
+large <- z [z>= 5]
 
 ### Bonus ###
 
 # Replace the values in `z` that are larger than 5 with the number 5
-
+larger <- z >= 5
+index <- 1
+for(value in larger){
+  if(value){
+    z[index] <- 5
+  }
+  index <- index + 1
+}
 
 # Replace every other value in `z` with the number 0
+other <- z <5
+index <- 1
+for(value in other){
+  if(value){
+    z[index] <- 0
+  }
+  index <- index + 1
+}
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,22 +1,48 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-
+marbles <- c('blue', 'red', 'white', 'green', 'yellow', 'black')
 
 # Use the `sample` function to select a single marble
-
+sample(marbles, 1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
-
+MarbleGame <- function(guess){
+  ans <- sample(marbles, 1)
+  if(ans == guess){
+    return (TRUE)
+  }
+  return (FALSE)
+}
 
 # Play the marble game!
-
+MarbleGame('black')
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 
+Play <- function(){
+  count <- 0
+  win <- FALSE
+  while(!win){
+    guess <- sample(marbles, 1)
+    win <- MarbleGame(guess)
+    count <- count + 1
+  }
+  return (count);
+}
+
+numGames <- Play()
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
+
+games <- 1000
+total.tries <- 0
+while(games > 0){
+  total.tries <- total.tries + Play()
+  games <- games - 1
+}
+average <- total.tries / 1000
